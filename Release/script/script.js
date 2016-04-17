b9b630eb029f7e4fa8221b6b1ed8d4d00563548c
Sokcuri <sokcuri@naver.com>
2016-04-17 23:45:02 +0900

    update to misc files

--- Release/script/script.js
+++ Release/script/script.js
@@ -1,6 +1,6 @@
-﻿// TweetDeck advanced javascript
-// Script only allow 'tweetdeck.twitter.com'
+﻿/* TweetDeck Player Script Extension */
+/* Script allow 'tweetdeck.twitter.com' */
 
-// force font-family change
+// font-family change
 //TDP.font = "굴림, Arial, Verdana, San-Serif";
-//TDP.injectStyles('.os-windows {font-family: ' + TDP.font);
+//TDP.injectStyles('.os-windows {font-family: ' + TDP.font+ '}');

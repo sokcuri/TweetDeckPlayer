025d5f26849b16b6031833de9fdaa5336f31791a
소쿠릿 <sokcuri@naver.com>
2016-08-01 15:28:47 +0900

    트윗덱 이미지 붙여넣기 스크립트 간소화

--- index.js
+++ index.js
@@ -247,65 +247,18 @@ ses.webRequest.onHeadersReceived(filter, (details, callback) => {
     win.webContents.on('did-finish-load', function() {
 
         // 가을별(Gaeulbyul) 님의 트윗덱 이미지 붙여넣기 스크립트
-        // 0.3b1 / 트윗덱에 클립보드 붙여넣기(Ctrl-V)로 이미지를 업로드하는 기능을 추가한다.
-        var gaeulbyul_paste = `var catcher = $('<div>')
-        .attr('contenteditable',true)
-        .css('opacity', 0)
-        .appendTo(document.body)
-        .focus();
-
-        function dataURIToBlob (dataURI) {
-            var [ mimeString, encodedData ] = dataURI.split(',');
-            var byteString;
-            if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-                byteString = atob(encodedData);
-            } else {
-                byteString = unescape(encodedData);
-            }
-            var type = mimeString.match(/^data:(.+);/)[1];
-            var ia = new Uint8Array(byteString.length);
-            for (let i = 0; i < byteString.length; i++) {
-                ia[i] = byteString.charCodeAt(i);
-            }
-            var blob = new Blob([ ia ], { type });
-            return blob;
+        // 트윗덱에 클립보드 붙여넣기(Ctrl-V)로 이미지를 업로드하는 기능을 추가한다.
+        // https://gist.github.com/zn/4f622ba80513e0f4d0dd3f13dcd085db
+        var gaeulbyul_paste = `$(document.body).on('paste', function (event) {
+        var items = event.originalEvent.clipboardData.items;
+        var item = items[0];
+        if (item.kind !== 'file') return;
+        var files = [ item.getAsFile() ];
+        // 트윗 입력창을 닫은 이후에 멘션 안 남게
+        if (!$('.app-content').hasClass('is-open')) {
+            $(document).trigger('uiComposeTweet', { type: 'tweet' });
         }
-
-        function waitClipboard () {
-            var cer = catcher[0];
-            var child = cer.childNodes && cer.childNodes[0];
-            if (child) {
-                if (child.tagName === 'IMG') {
-                    var file = dataURIToBlob(child.src);
-                    pasteFile([ file ]);
-                }
-                cer.innerHTML = '';
-            } else {
-                setTimeout(waitClipboard, 100);
-            }
-        }
-
-        function pasteFile (files) {
-            // 트윗 입력창을 닫은 이후에 멘션 안 남게
-            if (!$('.app-content').hasClass('is-open')) {
-                $(document).trigger("uiComposeTweet", { type: 'tweet' });
-            }
-            $(document).trigger('uiFilesAdded', { files });
-        }
-
-        $(document.body).on('paste', function (event) {
-            try {
-                var clipdata = event.originalEvent.clipboardData;
-                var items = clipdata.items;
-                var item = items[0];
-            } catch (e) {
-                catcher.focus();
-                setTimeout(waitClipboard, 300);
-                return;
-            }
-            if (item.kind !== 'file') return;
-            var file = [ item.getAsFile() ];
-            pasteFile(file);
+        $(document).trigger('uiFilesAdded', { files });
         });`
         
         var inject_style = `function injectStyles(rule) {

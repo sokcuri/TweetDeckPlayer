f1527b0bf8983e0feb114caf00d2bdf7374a6070
Gaeulbyul <aeiou256@gmail.com>
2017-01-29 05:07:30 +0900

    Handle multiple images, Add toolbar to viewer

--- src/preload_scripts/image-viewer.js
+++ src/preload_scripts/image-viewer.js
@@ -1,57 +1,69 @@
-const {GM_addStyle} = require('./playermonkey');
-
 const VIEWER_HTML = `
+  <nav class="tiv-toolbar">
+    <div class="tiv-btngroup-left">
+      <button class="tiv-button tiv-btn-prev">
+        Previous
+      </button>
+      <button class="tiv-button tiv-btn-next">
+        Next
+      </button>
+    </div>
+    <div class="tiv-btngroup-right">
+      <button class="tiv-button tiv-btn-close">
+        Close
+      </button>
+    </div>
+  </nav>
   <img class="tiv-image">
 `;
 
-const VIEWER_CSS = `
-  .tdp-image-viewer {
-    position: fixed;
-    display: none;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    padding: 30px 50px;
-    background-color: rgba(0, 0, 0, .9);
-    z-index: 999;
-    overflow-y: auto;
-  }
-  /* 노트:
-  * Chromium >= 53 지원을 위해 user-select속성에
-  * -webkit- prefix를 붙여야 함.
-  * 출처: http://caniuse.com/#feat=user-select
-  */
-  .tiv-image {
-    display: block;
-    margin: auto;
-    max-width: 100%;
-    -webkit-user-select: none;
-    user-select: none;
-  }
-`;
+function extractURL (img) {
+  const css = img.css('background-image');
+  const url = css
+    .replace(/^url\(['"]?/, '')
+    .replace(/['"]?\)$/, '');
+  return url;
+}
 
 class TDPImageViewer {
   constructor () {
+    this.images = [];
     const viewer = this.viewer = document.createElement('div');
     viewer.classList.add('tdp-image-viewer');
     viewer.innerHTML = VIEWER_HTML;
     const image = this.image = viewer.querySelector('img.tiv-image');
-    image.addEventListener('load', event => {
-      if (/:small$/.test(image.src)) {
-        image.src = image.src.replace(/:small$/, ':orig');
-      }
+    const toolbar = this.toolbar = viewer.querySelector('.tiv-toolbar');
+    toolbar.querySelector('.tiv-btn-prev').addEventListener('click', event => {
+      event.preventDefault();
+      this.prev();
     });
-    viewer.addEventListener('click', event => {
-      if (!(event.target.isSameNode(image))) {
-        window.$(document).trigger('tiv-close');
-      }
+    toolbar.querySelector('.tiv-btn-next').addEventListener('click', event => {
+      event.preventDefault();
+      this.next();
     });
-    GM_addStyle(VIEWER_CSS);
+    toolbar.querySelector('.tiv-btn-close').addEventListener('click', event => {
+      window.$(document).trigger('tiv-close');
+    });
     document.body.appendChild(viewer);
   }
-  showImage (url) {
-    this.image.src = url;
+  update () {
+    const {images, index} = this;
+    const length = images.length;
+    this.image.src = images[index].url;
+    const prev = this.toolbar.querySelector('.tiv-btn-prev');
+    const next = this.toolbar.querySelector('.tiv-btn-next');
+    prev.disabled = (index === 0);
+    next.disabled = (index === length - 1);
+  }
+  prev () {
+    this.index -= 1;
+    this.update();
+  }
+  next () {
+    this.index += 1;
+    this.update();
+  }
+  show () {
     this.viewer.style.display = 'block';
   }
   close () {
@@ -62,8 +74,11 @@ class TDPImageViewer {
 module.exports = function imageViewer () {
   const viewer = new TDPImageViewer;
   const $ = window.$;
-  $(document).on('tiv-show-image', (event, url) => {
-    viewer.showImage(url);
+  $(document).on('tiv-show-image', (event, parameter) => {
+    viewer.images = parameter.images;
+    viewer.index = parameter.index;
+    viewer.update();
+    viewer.show();
   });
   $(document).on('tiv-close', event => {
     viewer.close();
@@ -73,18 +88,30 @@ module.exports = function imageViewer () {
     // event.stopPropagation();
     event.stopImmediatePropagation();
     const target = $(event.currentTarget);
-    const img = target.find('img.media-img');
-    let url = '';
-    if (img.length !== 0) {
-      url = img.attr('src');
-    } else {
-      url = target.css('background-image');
-      url = url.replace(/^url\(['"]?/, '');
-      url = url.replace(/['"]?\)$/, '');
-    }
-    url = url.replace(/:small$/, ':orig');
-    if (url) {
-      $(document).trigger('tiv-show-image', url);
-    }
+    const container = target.parents('.media-preview');
+    const images = container.find('a[rel=mediaPreview]');
+    const parameter = {
+      index: 0,
+      images: [],
+    };
+    const targetImage = extractURL(target);
+    images.each((index, image) => {
+      image = $(image);
+      const img = image.find('img.media-img');
+      let url = '';
+      if (img.length !== 0) {
+        url = img.attr('src');
+      } else {
+        url = extractURL(image);
+      }
+      if (url === targetImage) {
+        parameter.index = index;
+      }
+      url = url.replace(/:small$/, ':orig');
+      parameter.images.push({
+        index, url,
+      });
+    });
+    $(document).trigger('tiv-show-image', parameter);
   });
 };

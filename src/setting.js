db3730693910dab6e74c26c20db94cb619f4f759
Dalgona <dalgona@hontou.moe>
2017-04-04 00:29:07 +0900

    설정 UI와 데이터 분리 (#100)
    Automatically generate the settings page
    
    * Automatically generate the Settings page
    
    * Migrate config (as a result of rebasing)
    
    * Remove temporary codes

--- src/setting.js
+++ src/setting.js
@@ -1,5 +1,8 @@
 const electron = require('electron');
+const noUiSlider = require('nouislider');
 const {ipcRenderer} = electron;
+
+const schema = require('./config-schema');
 
 function saveConfig (config) {
   ipcRenderer.send('save-config', config);
@@ -12,6 +15,8 @@ function loadConfig () {
 var config;
 
 function onload () {
+  initializeComponents();
+
   config = loadConfig();
   var wrapper = document.getElementById('wrapper');
   // 트윗덱 테마를 바탕으로 설정창 테마 변경.
@@ -66,3 +71,94 @@ document.addEventListener('DOMContentLoaded', onload);
 window.addEventListener('beforeunload', () => {
   saveConfig(config);
 });
+
+function initializeComponents () {
+  let form = document.querySelector('#settingform > section');
+  for (let obj of schema) {
+    switch (obj._type) {
+      case 'section':
+        var e = document.createElement('header');
+        e.innerHTML = `<span>${obj.label}</span>`;
+        form.appendChild(e);
+        break;
+      case 'subsection':
+        var e = document.createElement('div');
+        e.className = 'settings-item sub-header';
+        e.innerHTML = obj.label;
+        form.appendChild(e);
+        break;
+      case 'entry':
+        initializeEntries(obj, form);
+        break;
+      default:
+        console.warn(`Unrecognized item type: '${obj._type}'`);
+        break;
+    }
+  }
+}
+
+function initializeEntries(entry, form) {
+  let e = document.createElement('div');
+  e.className = 'settings-item';
+
+  switch (entry.valueType) {
+    case 'bool':
+      e.innerHTML = `<label><input type="checkbox" id="${entry.name}"><label for="${entry.name}"><div></div></label>${entry.label}</label>`;
+      break;
+    case 'text':
+      e.innerHTML = `<input type="text" id="${entry.name}">`;
+      break;
+    case 'longtext':
+      e.innerHTML = `<textarea id="${entry.name}" rows="5"></textarea>`;
+      break;
+    case 'enum':
+      let opts = entry.options.map(x => `<option value="${x.value}">${x.label}</option>`).join('');
+      e.innerHTML = `<select name="${entry.name}" id="${entry.name}">${opts}</select>`;
+      break;
+    case 'number': {
+      e.innerHTML = `<div id="${entry.name}Slider"></div><div><input type="text" id="${entry.name}"></div>`;
+      let slider = e.querySelector(`#${entry.name}Slider`);
+      let text = e.querySelector(`#${entry.name}`);
+      createSlider(entry, slider, text);
+    } break;
+    default:
+      console.warn(`Unrecognized entry value type: '${entry.valueType}'`);
+      break;
+  }
+  form.appendChild(e);
+
+  if (entry.description) {
+    let e = document.createElement('div');
+    e.className = 'settings-item description' + (entry.valueType == 'bool' ? ' for-checkbox' : '');
+    e.innerHTML = entry.description;
+    form.appendChild(e);
+  }
+}
+
+function createSlider (entry, slider, text) {
+  noUiSlider.create(slider, {
+    start: [ require('./config').load()[entry.name] || 0 ],
+    step: entry.step,
+    range: {
+      min: [ entry.min ],
+      max: [ entry.max ],
+    },
+    format: {
+      to (value) { return Math.floor(value); },
+      from (value) { return value; },
+    },
+  });
+  var tTime;
+  slider.noUiSlider.on('update', (values, handle) => {
+    text.value = values[handle];
+    if (!tTime || tTime + 100 < new Date().getTime()) {
+      var event = new Event('change');
+      var settingForm = document.getElementById('settingform');
+      settingForm.dispatchEvent(event);
+      tTime = new Date().getTime();
+    }
+  });
+  text.addEventListener('change', e => {
+    slider.noUiSlider.set(e.target.value);
+  });
+}

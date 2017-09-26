b51c20148f9a1557c4f3ec297d09a8e06785fd5a
OnLiU <jhs_onliu@naver.com>
2017-09-27 04:58:46 +0900

    separate menu template

--- src/index.js
+++ src/index.js
@@ -493,74 +493,7 @@ app.on('ready', () => {
   ipcMain.on('run', event => run(chk_win));
 
   // MacOS Application menu
-  const template = [
-    {
-      label: 'Edit',
-      submenu: [
-        {
-          role: 'undo',
-        },
-        {
-          role: 'redo',
-        },
-        {
-          type: 'separator',
-        },
-        {
-          role: 'cut',
-        },
-        {
-          role: 'copy',
-        },
-        {
-          role: 'paste',
-        },
-        {
-          role: 'selectall',
-        },
-        {
-          role: 'delete',
-        },
-      ],
-    },
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.reload();
-          },
-        },
-        {
-          role: 'togglefullscreen',
-        },
-        {
-          type: 'separator',
-        },
-        {
-          role: 'resetzoom',
-        },
-        {
-          role: 'zoomin',
-        },
-        {
-          role: 'zoomout',
-        },
-        {
-          type: 'separator',
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.webContents.toggleDevTools();
-          },
-        },
-      ],
-    },
-  ];
+  const template = require('./MacAppMenu');
 
   if (process.platform === 'darwin') {
     template.unshift({

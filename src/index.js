c0bdc94386facb35c030ec5afdac637c7421775e
sokcuri <sokcuri@naver.com>
2017-10-06 12:17:41 +0900

    Revert "Remove Character-count hidden css"
    This reverts commit f85f1861893bf989dbd19e9a29f54b57d1f64647.

--- src/index.js
+++ src/index.js
@@ -645,6 +645,15 @@ app.on('ready', () => {
     `);
     win.webContents.send('apply-config');
 
+    // character-count default to hidden
+    win.webContents.insertCSS(`
+    .js-character-count {
+      opacity: 0;
+    }
+    .tdp-show {
+      opacity: 1;
+    }`);
+
     // 유저 스크립트 로딩
     const noop = () => {};
     const loadUserAssets = async () => {

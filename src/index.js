954075a16f4e9e28c1f04d4f60ca6a48db928141
sokcuri <sokcuri@naver.com>
2017-10-06 16:16:15 +0900

    versionCompare fix

--- src/index.js
+++ src/index.js
@@ -908,49 +908,46 @@ ipcMain.on('nogpu-relaunch', () => {
 // JS version number compare
 // electron 버전 비교를 위해서 삽입
 // http://stackoverflow.com/questions/6832596/how-to-compare-software-version-number-using-js-only-number
+
 const versionCompare = (v1, v2, options) => {
-  const lexicographical = options && options.lexicographical;
-  const zeroExtend = options && options.zeroExtend;
-  let v1parts = v1.split('.');
-  let v2parts = v2.split('.');
+  var lexicographical = options && options.lexicographical,
+    zeroExtend = options && options.zeroExtend,
+    v1parts = v1.split('.'),
+    v2parts = v2.split('.');
 
-  const isValidPart = x => {
-    return (lexicographical)
-      ? /^\d+[A-Za-z]*$/
-      : /^\d+$/.test(x);
-  };
-
-  const isUnvalid = v1parts.every(isValidPart) && v2parts.every(isValidPart);
-  if (isUnvalid) return NaN;
-
-  if (zeroExtend) {
-    while (v1parts.length < v2parts.length) v1parts.push('0');
-    while (v2parts.length < v1parts.length) v2parts.push('0');
+  function isValidPart(x) {
+    return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
   }
-
+  if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+    return NaN;
+  }
+  if (zeroExtend) {
+    while (v1parts.length < v2parts.length) v1parts.push("0");
+    while (v2parts.length < v1parts.length) v2parts.push("0");
+  }
   if (!lexicographical) {
     v1parts = v1parts.map(Number);
     v2parts = v2parts.map(Number);
   }
-
-  for (let i = 0; i < v1parts.length; ++i) {
-    if (v2parts.length === i) {
+  for (var i = 0; i < v1parts.length; ++i) {
+    if (v2parts.length == i) {
       return 1;
     }
-
-    if (v1parts[i] === v2parts[i]) {
+    if (v1parts[i] == v2parts[i]) {
       continue;
-    } 
-
-    return (v1parts[i] > v2parts[i]) ? 1 : -1;
+    }
+    else if (v1parts[i] > v2parts[i]) {
+      return 1;
+    }
+    else {
+      return -1;
+    }
   }
-
-  if (v1parts.length !== v2parts.length) {
+  if (v1parts.length != v2parts.length) {
     return -1;
   }
-
   return 0;
-};
+}
 
 // 컨텍스트 메뉴
 ipcMain.on('context-menu', (event, menu, isRange, Addr, isPopup) => {
